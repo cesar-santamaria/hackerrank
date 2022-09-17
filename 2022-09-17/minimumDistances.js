a0df6667ca013fcc3db4247068ed11e1b926a6ae
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-09-17 10:30:19 +0100

    Complete coding challenge minimum distance.

---
+++ 2022-09-17/minimumDistances.js
@@ -0,0 +1,12 @@
+function minimumDistances(a) {
+  // Write your code here
+  let min = [];
+  for (let i = 0; i < a.length; i++) {
+      for (let j = i + 1; j < a.length; j++) {
+          if (a[i] === a[j]) {
+              min.push(Math.abs(i - j))
+          }
+      }
+  }
+  return min.length ? Math.min(...min) : -1
+}
